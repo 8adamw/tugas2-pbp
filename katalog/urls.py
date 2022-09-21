11ea62b28c0292fee5043f4b9683d37ab439a9ca
8adamw <104297172+8adamw@users.noreply.github.com>
2022-09-22 04:26:33 +0700

    Test commit 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,11 @@
 # TODO: Implement Routings Here
+
+from django.urls import path
+from katalog.views import show_katalog
+
+app_name = 'wishlist'
+
+urlpatterns = [
+    path('', show_katalog, name='show_katalog'),
+    path('katalog/', include('katalog.urls')),
+]

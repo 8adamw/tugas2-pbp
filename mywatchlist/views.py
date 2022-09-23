7013da6b45970e1b3d99ccfceb85994c3736e8bc
8adamw <104297172+8adamw@users.noreply.github.com>
2022-09-23 10:14:47 +0700

    Submisi Tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from mywatchlist.models import Movie
+
+# TODO: Create your views here.
+
+data_barang_katalog = Movie.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Adam'
+}
+
+def show_mywishlist(request):
+    return render(request, "mywishlist.html", context)
